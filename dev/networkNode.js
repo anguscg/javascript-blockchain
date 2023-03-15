203e95765511e169ab958b65f8c9392fec2329c7
anguscg <angus.cg@hotmail.com>
2023-03-16 10:34:41 +1100

    fixed register and broadcast endpoint. Kept crashing the  server

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -44,62 +44,62 @@ app.get('/mine', function(req, res) {
 });
 // registering node and broadcast to network
 app.post('/register-and-broadcast-node', function (req, res) {
-    const newNodeUrl = req.body.newNodeUrl
-    if (bitcoin.networkNodes.indexOf(newNodeUrl) == -1)
-      bitcoin.networkNodes.push(newNodeUrl)
-  
-    const regNodesPromises = []
-    bitcoin.networkNodes.forEach(networkNodeUrl => {
-      const requestOptions = {
-        url: networkNodeUrl + '/register-node',
-        method: 'POST',
-        data: { newNodeUrl: newNodeUrl },
-      }
-  
-      regNodesPromises.push(axios(requestOptions))
-    })
-  
-    Promise.all(regNodesPromises)
-      .then(data => {
-        const bulkRegisterOptions = {
-          url: newNodeUrl + '/register-nodes-bulk',
-          method: 'POST',
-          data: {
-            allNetworkNodes: [...bitcoin.networkNodes, bitcoin.currentNodeUrl],
-          },
-        }
-  
-        return axios(bulkRegisterOptions)
-      })
-      .then(data => {
-        return res.json({
-          note: 'New node registered with network successfully.',
-        })
-      })
+  const newNodeUrl = req.body.newNodeUrl
+  if (bitcoin.networkNodes.indexOf(newNodeUrl) == -1)
+    bitcoin.networkNodes.push(newNodeUrl)
+
+  const regNodesPromises = []
+  bitcoin.networkNodes.forEach(networkNodeUrl => {
+    const requestOptions = {
+      url: networkNodeUrl + '/register-node',
+      method: 'POST',
+      data: { newNodeUrl: newNodeUrl },
+    }
+
+    regNodesPromises.push(axios(requestOptions))
   })
+
+  Promise.all(regNodesPromises)
+    .then(data => {
+      const bulkRegisterOptions = {
+        url: newNodeUrl + '/register-nodes-bulk',
+        method: 'POST',
+        data: {
+          allNetworkNodes: [...bitcoin.networkNodes, bitcoin.currentNodeUrl],
+        },
+      }
+
+      return axios(bulkRegisterOptions)
+    })
+    .then(data => {
+      return res.json({
+        note: 'New node registered with network successfully.',
+      })
+    })
+})
   
 // this end point will register the new node with the node that recieves the request
 app.post('/register-node', function (req, res) {
-    const newNodeUrl = req.body.newNodeUrl
-    const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) == -1
-    const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl
-    if (nodeNotAlreadyPresent && notCurrentNode)
-      bitcoin.networkNodes.push(newNodeUrl)
-    return res.json({ note: 'New node registered successfully.' })
-  })
+  const newNodeUrl = req.body.newNodeUrl;
+  const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) == -1
+  const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl;
+  if (nodeNotAlreadyPresent && notCurrentNode)
+    bitcoin.networkNodes.push(newNodeUrl)
+  return res.json({ note: 'New node registered successfully.' })
+})
 // registering multiple nodes
 app.post('/register-nodes-bulk', function (req, res) {
-    const allNetworkNodes = req.body.allNetworkNodes
-    allNetworkNodes.forEach(networkNodeUrl => {
-      const nodeNotAlreadyPresent =
-        bitcoin.networkNodes.indexOf(networkNodeUrl) == -1
-      const notCurrentNode = bitcoin.currentNodeUrl !== networkNodeUrl
-      if (nodeNotAlreadyPresent && notCurrentNode)
-        bitcoin.networkNodes.push(networkNodeUrl)
-    })
-  
-    return res.json({ note: 'Bulk registration successful.' })
+  const allNetworkNodes = req.body.allNetworkNodes
+  allNetworkNodes.forEach(networkNodeUrl => {
+    const nodeNotAlreadyPresent =
+      bitcoin.networkNodes.indexOf(networkNodeUrl) == -1
+    const notCurrentNode = bitcoin.currentNodeUrl !== networkNodeUrl
+    if (nodeNotAlreadyPresent && notCurrentNode)
+      bitcoin.networkNodes.push(networkNodeUrl)
   })
+
+  return res.json({ note: 'Bulk registration successful.' })
+})
 
 app.listen(port, function() {
     console.log(`Listening on port ${port}...`);
